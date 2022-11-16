a7652f1baddfc567e133f6184140a104a66cbec0
nazarenafalleti <epet20nazarenafalleti@gmail.com>
2022-11-16 10:13:09 -0300

    subir js

--- JS/script.js
+++ JS/script.js
@@ -1,7 +1,26 @@
+const boton=document.getElementById('boton');
+const texto=document.getElementById('texto');
+function addElemento(texto) {
+  campo = document.getElementById("campo");
+  h1 = document.createElement("h1");
+  h1.innerHTML = texto;
+  campo.appendChild(h1);
+}
+boton.classList.add('mover')
 
+texto.addEventListener('keyup', ()=> {
+  textoo=texto.value.length;
+  if((textoo>0 && textoo<=30  )){
+      boton.classList.remove('mover');
 
+  }
+})
+boton.addEventListener('click' ,()=> {
+    html.classList.add('mover');
+    boton.classList.add ('denuevo')
+  })
 
-
+  
 
 
 
